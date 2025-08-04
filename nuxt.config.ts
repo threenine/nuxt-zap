bb7d3bf9e57427aec10250bfe4130f4d16a9d6fb
Gary Woodfine <lnb0l9dc@duck.com>
2025-08-04 18:10:48 +0100

    **chore: configure project with standards and dependencies**

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/scripts',
+    '@nuxt/test-utils',
+    '@nuxt/ui',
+  ],
+})
